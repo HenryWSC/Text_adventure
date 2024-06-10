7641feeefa38f4a99efd1daa79e836d2701faa70
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-06-11 11:41:08 +1200

    intro up to bear encounter

---
+++ jac.py
@@ -0,0 +1,15 @@
+#intro
+start = input("welcome adventurer are you ready to enter the world of the infinity acres")
+if start == "yes":
+    print("i hope you survive")
+else:
+    print("i pray for your death")
+
+#explaining the game
+print("during this adventure you will have to battle monsters and the environment in order to survive")
+print("your goal is to make it to duras, the royal capital")
+print("you have 20 hp to start find armor to increase it")
+
+#start
+print("you find yourself in a forest")
+
