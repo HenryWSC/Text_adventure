4754891885e9320fb1180cf0e92aca20895a915d
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-06-11 12:29:16 +1200

    Update jac.py

--- jac.py
+++ jac.py
@@ -12,4 +12,38 @@ print("you have 20 hp to start find armor to increase it")
 
 #start
 print("you find yourself in a forest")
+question = "what direction will you go"
+a = "north"
+b = "east"
+c = "west"
+answer = input("{}\nA.{} B.{} C.{}".format(question, a, b, c)).lower()
 
+if answer == a or answer == "a":
+    print("you spot a bear running towards you")
+elif answer == "":
+    print("you spot a bear running towards you")
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
+    print("you spot a bear running towards you")
+else:
+    print("you spot a bear running towards you")
+
+
+#bear encounter
+print("he is getting closer")
+question = "what will you do"
+a = "fight"
+b = "run"
+c = "bag"
+answer = input("{}\nA.{} B.{} C.{}".format(question, a, b, c)).lower()
+
+if answer == a or answer == "a":
+    print("you land a clean right hook across the bears face, he is unfased.")
+    print("the bear slashes your stomach, your guts spill out and you die")
+elif answer == b or answer == "b":
+    print("you are able to get away from the bear")
+elif answer == c or answer == "c":
+    print("you have no items")
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
+    print("")
+else:
+    print("thats not an answer")
