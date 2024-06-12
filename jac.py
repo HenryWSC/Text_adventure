5e3a1723aadd524bab06e973ef3de36c93ebbc44
jacksonsmortypants <160076803+jacksonsmortypants@users.noreply.github.com>
2024-06-13 11:21:31 +1200

    Update jac.py

--- jac.py
+++ jac.py
@@ -1,3 +1,4 @@
+hp = 20
 #intro
 start = input("welcome adventurer are you ready to enter the world of the infinity acres")
 if start == "yes":
@@ -36,14 +37,25 @@ b = "run"
 c = "bag"
 answer = input("{}\nA.{} B.{} C.{}".format(question, a, b, c)).lower()
 
-if answer == a or answer == "a":
-    print("you land a clean right hook across the bears face, he is unfased.")
-    print("the bear slashes your stomach, your guts spill out and you die")
-elif answer == b or answer == "b":
-    print("you are able to get away from the bear")
-elif answer == c or answer == "c":
-    print("you have no items")
-elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
-    print("")
-else:
-    print("thats not an answer")
+while True:
+    if answer == a or answer == "a":
+        print("you land a clean right hook across the bears face, he is unfased.")
+        print("the bear slashes your stomach, your guts spill out and you die")
+        hp < 1
+    elif answer == b or answer == "b":
+        print("you are able to get away from the bear")
+        break
+    elif answer == c or answer == "c":
+        print("you have no items")
+    elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
+        print("")
+    else:
+        print("thats not an answer")
+
+
+print ("nice job")
+
+
+QES = ["you find yourself in a forest, what will you do"]
+
+OPT = ["north","east","west"]
