de0a76843985596557ad08327fd4cfc08dcf08e6
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-06-13 12:29:11 +1200

    finsshed bear and starting west and beach

--- jac.py
+++ jac.py
@@ -1,4 +1,3 @@
-hp = 20
 #intro
 start = input("welcome adventurer are you ready to enter the world of the infinity acres")
 if start == "yes":
@@ -13,6 +12,7 @@ print("you have 20 hp to start find armor to increase it")
 
 #start
 print("you find yourself in a forest")
+
 question = "what direction will you go"
 a = "north"
 b = "east"
@@ -34,28 +34,52 @@ print("he is getting closer")
 question = "what will you do"
 a = "fight"
 b = "run"
-c = "bag"
-answer = input("{}\nA.{} B.{} C.{}".format(question, a, b, c)).lower()
+answer = input("{}\nA.{} B.{}".format(question, a, b)).lower()
 
 while True:
     if answer == a or answer == "a":
         print("you land a clean right hook across the bears face, he is unfased.")
         print("the bear slashes your stomach, your guts spill out and you die")
-        hp < 1
+        break
     elif answer == b or answer == "b":
         print("you are able to get away from the bear")
-        break
-    elif answer == c or answer == "c":
-        print("you have no items")
-    elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
+    elif answer != a and answer != "a" and answer != b and answer != "b" and answer:
         print("")
     else:
         print("thats not an answer")
+    
+# major crossroad north,east and west
+    print("you find yourself further into the woods")
+    question2 = "where will you go"
 
-
-print ("nice job")
-
-
-QES = ["you find yourself in a forest, what will you do"]
-
-OPT = ["north","east","west"]
+             ##########################                        # 
+                                      #                        #    
+                                      #                        #     
+                                      #                        #    
+                                      #                        #   
+                                      #                        #    
+                                      #                        #     
+                                      #                        #    
+             ###################################################  
+                                      #    
+                                      #
+                                      #
+                                      #
+                                      #    
+                                      #   
+                                      #    
+                                      #    
+                                      #    
+                                      ########################## 
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    break
--- main.py
+++ main.py
@@ -1,1 +1,73 @@
+#intro
+start = input("welcome adventurer are you ready to enter the world of the infinity acres")
+if start == "yes":
+    print("i hope you survive")
+else:
+    print("i pray for your death")
 
+#explaining the game
+print("during this adventure you will have to battle monsters and the environment in order to survive")
+print("your goal is to make it to duras, the royal capital")
+print("you have 20 hp to start find armor to increase it")
+
+#start
+print("you find yourself in a forest")
+
+question = "what direction will you go"
+a = "north"
+b = "east"
+c = "west"
+answer = input("{}\nA.{} B.{} C.{}".format(question, a, b, c)).lower()
+
+if answer == a or answer == "a":
+    print("you spot a bear running towards you")
+elif answer == "":
+    print("you spot a bear running towards you")
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer:
+    print("you spot a bear running towards you")
+else:
+    print("you spot a bear running towards you")
+
+
+#bear encounter
+print("he is getting closer")
+question = "what will you do"
+a = "fight"
+b = "run"
+answer = input("{}\nA.{} B.{}".format(question, a, b)).lower()
+
+while True:
+    if answer == a or answer == "a":
+        print("you land a clean right hook across the bears face, he is unfased.")
+        print("the bear slashes your stomach, your guts spill out and you die")
+        break
+    elif answer == b or answer == "b":
+        print("you are able to get away from the bear")
+    elif answer != a and answer != "a" and answer != b and answer != "b" and answer:
+        print("")
+    else:
+        print("thats not an answer")
+    
+    print("you have found yourself further into the woods")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    break
