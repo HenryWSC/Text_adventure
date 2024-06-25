25d184cfe7cdb21bf28ba1a305455c8160d8da25
Heeeeeeeeeeen <livetth@wsc.school.nz>
2024-06-25 12:28:16 +1200

    almost finished beach

--- hen.py
+++ hen.py
@@ -1,182 +1,5 @@
-#starting beach real
-STORY = [["you travel to duras","you find a small wrecked boat, you investigat the boat and find string, a stick, a sharp stone and some wood kindle",
-              "do you make a torch, spear or fire", "you make a fire",
-              "smoke plums from the fire and could attract animals, do you put it out?",
-              "you keep the fire going and after a while you hear wolves howling and you hear shuffling behind you. You look behind and see 5 wolves coming out the forest",
-              "you fight", "you fight", "you fight", "you fight",
-              "you die from the wolves"]
-              [#lighthouse 
-              "you enter the lighthouse and find a letter telling you to get to the kingdom of duras ASAP", 
-               "you start to feel hungry, do you go to the forest to find food or go to duras?",
-               "you see 3 goblins is the distance blocking the entrance to the duras kingdom, do you approach them or do into the forest?"
-               #setting sun
-               "you go into the forest to look for food and you find a bush of berries"
-               #smoke plums
-               "you put out the fire and it starts to get cold since its now midnight do you go get food or go to sleep", 
-               "you die from hyperthermia",
-               #wolf attack 
-               "you run from the wolf attacking you and enter the woods you survive on 3 HP" ]]
-
-OPTIONS = [["enter the lighthouse", "go along the beach"],
-           ["go to duras", "go to the forest to look for food"],
-           ["continue along the beach", "enter the lighthouse"],
-           ["go back to the light house","investigate the boat"],
-            ["make a fire", "make a spear", "make a torch"],
-            ["look for food", "make fire"],
-            ["put out the fire", "keep the fire going"],
-            ["put out the fire", "keep the fire going despite the warning"],
-            ["run", "fight"]]
-SHORT_OPTIONS =["a", "b", "c"]
-ANSWERS = []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+has_spear = False
+has_torch = False
 #starting beach
 print("you run throught the woods and see a break in the trees, as you break throught the tree lines you see a beach and hear crashes of waves. you seem safe.")
 
@@ -185,8 +8,13 @@ print("as you walk along the beach you come across a light house it seems old an
 H_question = "what will you do"
 a = "continue walking along the beach"
 b = "go into the lighthouse"
-answer = input("{}\nA.{} B.{}".format(H_question, a, b)).lower()
+answer = input("{}\nA.{} B.{}".format(H_question, a, b,)).lower()
 
+#lighthouse start
+if answer == a or answer == "a":
+        print("you walk into the lighthouse and find a chest you open it and you find a letter telling you to go to duras")
+        print("you feel compelled to go to duras and when you do you find 3 golblins blocking your path")
+#lighthouse ends
 
 if answer == a or answer == "a":
         print("you continue walking along the beach until you find a wrecked rowboat")
@@ -195,57 +23,375 @@ if answer == a or answer == "a":
         H_question2 = "what will you do"
 a = "Investigate the boat"
 b = "go back to light house"
-answer = input("{}\nA.{} B.{}".format(H_question, a, b)).lower()
+answer = input("{}\nA.{} B.{}".format(H_question, a, b,)).lower()
+
 if answer == a or answer == "a":
         print("you investigate the row boat and find string, a stick, a sharp flint and wood kindle")
 #Investigate row boat
 H_question3 = "what will you do"
-a = "make a fire"
-b = "make a spear"
-c = "make a torch"
-answer = input("{}\nA.{} B.{} C.{}".format(H_question, a, b)).lower()
-
-
-if answer == a or answer == "a":
-        print("you continue walking along the beach until you find a wrecked rowboat")
-       
-        #wrecked boat
-       
-if answer == a or answer == "a":
-       print("you continue walking along the beach until you find a wrecked rowboat")
-       
-elif answer == b or answer == "b":
-       print("you enter the lighthouse and you see a small chest, you open it and find a scroll telling you to get to Duras as quickly as possible")
-elif answer != a and answer != "a" and answer != b and answer != "b" and answer:
-       print("")
-else:
-             
-       print("thats not an answer")
+a = "make a spear"
+b = "make a torch"
+answer = input("{}\nA.{} B.{}".format(H_question, a, b,)).lower()
 #Investigate row boat ends
-       
+
+#making spear
 if answer == a or answer == "a":
-       print("you continue walking along the beach until you find a wrecked rowboat")
-       
-elif answer == b or answer == "b":
-       print("you enter the lighthouse and you see a small chest, you open it and find a scroll telling you to get to Duras as quickly as possible")
-elif answer != a and answer != "a" and answer != b and answer != "b" and answer:
-       print("")
-else:
-             
-       print("thats not an answer")
-#wrecked boat
-       
+        has_torch = True
+        print("you make a torch by wrapping string around a stick and lighting it with a flint, but the sun starts to set")
+
+H_question3 = "what will you do"
+a = "make a fire"
+b = "go look for berries"
+answer = input("{}\nA.{} B.{}".format(H_question, a, b,)).lower()
+#making spear ends
+#making spear
+if answer == b or answer == "b":
+        has_spear = True
+        print("you make a spear out of the flint and sticks but the sun starts to set")
+
+H_question3 = "what will you do"
+a = "make a fire"
+b = "go look for berries"
+answer = input("{}\nA.{} B.{}".format(H_question, a, b,)).lower()
+#making spear ends
+
+#making fire
 if answer == a or answer == "a":
-       print("you continue walking along the beach until you find a wrecked rowboat")
-       
+        print("you make a fire smoke plums up and may attract predetors")
+
 elif answer == b or answer == "b":
-       print("you enter the lighthouse and you see a small chest, you open it and find a scroll telling you to get to Duras as quickly as possible")
-elif answer != a and answer != "a" and answer != b and answer != "b" and answer:
-       print("")
+        print("what do you do?")
+        
+        a = "put out the fire"
+        b = "leave the fire going"
+        answer = input("{}\nA.{} B.{}".format(H_question, a, b, )).lower()
+#making fire ends
+
+#wolves encounter
+if answer == a or answer == "a":
+        question = "3 wolves appear behind you from the forest"
+a = "run away"
+b = "fight"
+answer = input("{}\nA.{} B.{}".format(question, a, b)).lower()
+
+if answer == a or answer == "a":
+    print("you run away into the woods until you find a cabin")
+#go into jacksons
+    
+elif answer ==  b or answer == "b":
+    print("as you fight the wolfes they outnumber and overwhelm you and you die")
+    
 else:
-             
-       print("thats not an answer")
+    print("that is not an answer")
+
+#going for berries
+if answer == b or answer == "b":
+        print("you find a bush of berries")
+
+        question3 = "what would you like to do now "
+        a = "gather berries"
+        b = "go deeper into the woods"
+        answer = input("{}\nA.{} B.{}".format(H_question, a, b, )).lower()
+#berries go to jackson
        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#starting beach real
+#STORY = [["you travel to duras","you find a small wrecked boat, you investigat the boat and find string, a stick, a sharp stone and some wood kindle",
+#              "do you make a torch, spear or fire", "you make a fire",
+ #             "smoke plums from the fire and could attract animals, do you put it out?",
+  #            "you keep the fire going and after a while you hear wolves howling and you hear shuffling behind you. You look behind and see 5 wolves coming out the forest",
+   ##          "you die from the wolves"]
+     #         [#lighthouse 
+      #        "you enter the lighthouse and find a letter telling you to get to the kingdom of duras ASAP", 
+       ####     "you go into the forest to look for food and you find a bush of berries"
+           #    #smoke plums
+            #   "you put out the fire and it starts to get cold since its now midnight do you go get food or go to sleep", 
+             #  "you die from hyperthermia",
+               #wolf attack 
+              # "you run from the wolf attacking you and enter the woods you survive on 3 HP" ]]
+
+#OPTIONS = [["enter the lighthouse", "go along the beach"],
+ #          ["go to duras", "go to the forest to look for food"],
+  #         ["continue along the beach", "enter the lighthouse"],
+   #        ["go back to the light house","investigate the boat"],
+    #        ["make a fire", "make a spear", "make a torch"],
+     #       ["look for food", "make fire"],
+      #      ["put out the fire", "keep the fire going"],
+       #    ["run", "fight"]]
+#SHORT_OPTIONS =["a", "b", "c"]
+#ANSWERS = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
